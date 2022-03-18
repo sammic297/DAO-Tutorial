1a3611cf123c57338fb39242c0f5fdc4a30c7e08
Odey, Samuel Michael <sammic297@gmail.com>
2022-03-18 21:04:50 +0100

    Relocate constants.js
    Relocate constants.js to hardhat-tutorial folder and delete constants folder

--- hardhat-tutorial/constants/constants.js
+++
@@ -1,5 +0,0 @@
-// Replace the value with your NFT contract address
-const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
-    "0xc535B249bc0714eE21B4DDf47A33170e3A467E20";
-
-module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
